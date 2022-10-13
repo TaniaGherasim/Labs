e0d74b2634c28e3aa3b6baf50fc40d8533dca358
Tania Gherasim <taniaursan@gmail.com>
2022-10-13 13:08:00 -0400

    javascript-refresher

---
+++ Javascript-refresher/script.js
@@ -0,0 +1,22 @@
+var firstName = "Tania";
+var lastName = "Gherasim";
+var fullName = firstName.concat(lastName);
+console.log(fullName);
+
+var population = "672351"
+console.log("population");
+
+let x = 672351;
+let y = 3;
+let = x * y;
+
+var javascriptIsCool = "true";
+
+var colors = ["Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet"];
+
+for (var i=1; i<6; i++) {
+    console.log(i);
+}
+for (var i=0; i<5; i++) {
+    console.log(i);
+}
