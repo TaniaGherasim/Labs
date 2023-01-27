6e48d295610f017d7430c5589e53784d8b658f2f
Tania Gherasim <taniaursan@gmail.com>
2023-01-26 21:56:29 -0500

    Ice cream lab pt 1

---
+++ Lab 14-Ice cream pt.1/myapp/src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Header } from './components/Header';
+import { AdDesign } from './components/AdDesign';
+import { Votes } from './components/Votes';
+
+function App() {
+  return (
+    <>
+    <Header />
+    <div className="main">
+    <AdDesign />
+    <Votes />
+    </div> 
+    </>
+   
+  );
+}
+
+export default App;
